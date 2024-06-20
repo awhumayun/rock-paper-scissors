a05cb335d510f1a90bdbff0a1b1aca101255eeb2
awhumayun <abd.humayun@gmail.com>
2024-06-20 22:11:26 +0000

    Add player code to beat the bots

--- RPS.py
+++ RPS.py
@@ -1,10 +1,16 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+def player(previous_play, opponent_history=[], play_statistics={}):
+    opponent_history.append(previous_play if previous_play else 'R')
+    
+    guess = 'P'
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) > 4:
+        last_five_plays = "".join(opponent_history[-5:])
+        play_statistics[last_five_plays] = play_statistics.get(last_five_plays, 0) + 1      
+        potential_plays = ["".join(opponent_history[-4:] + [v]) for v in ['R', 'P', 'S']]
 
-    return guess
+        if sub_statistics := {key: play_statistics.get(key, 0) for key in potential_plays if key in play_statistics}:
+            guess = max(sub_statistics, key=sub_statistics.get)[-1]
+
+    return {'P': 'S', 'R': 'P', 'S': 'R'}.get(guess, 'R')
